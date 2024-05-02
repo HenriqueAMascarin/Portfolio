130125a452088fde31d8f66c10b5ecf6b6e74fdc
HenriqueAMascarin <henriqueamascarin@gmail.com>
2024-05-02 11:04:17 -0300

    scrollView-unique-urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+import { useEffect } from "react";
 import Main from "./components/mainComponent/Main";
 import Thanks from "./components/thanksComponents/Thanks";
-import { Routes, Route} from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
+import { arrayCards } from "./components/mainComponent/projects/cardsData";
 
 function App() {
 
+  const location = useLocation();
+
+  useEffect(() => {
+    arrayCards.forEach((el) => {
+      if (location.hash.includes(el.id)) {
+        const locationId = location.hash.slice(location.hash.indexOf('#') + 1, location.hash.length);
+        document.getElementById(locationId)?.scrollIntoView();
+      }
+    })
+    
+  }, [])
+
   return (
-   <>
-    <Routes>
-      <Route path="/" element={<Main/>} />
-      <Route path="/obrigado" element={<Thanks/>}/>
-    </Routes>
-   </>
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/obrigado" element={<Thanks />} />
+      </Routes>
+    </>
   )
 }
 
